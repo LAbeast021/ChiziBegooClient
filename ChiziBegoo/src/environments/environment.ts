7316162cf84dcee31f94f83f6e17519896328920
Kiarash Hesampour <54046939+LAbeast021@users.noreply.github.com>
2024-05-17 15:44:04 -0700

    Update environment.ts

--- ChiziBegoo/src/environments/environment.ts
+++ ChiziBegoo/src/environments/environment.ts
@@ -1,5 +1,3 @@
 export const environment = {
-    baseUrl: "https://ec2-54-67-80-93.us-west-1.compute.amazonaws.com" , 
-    googleClientId : "502793030323-v19jmmdfto8v818tumtcsr1uh8b5t66q.apps.googleusercontent.com",
-    googleClientSecret : "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"
+  
 };
